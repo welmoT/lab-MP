94771d49c47c8d9b325cac0f77ae3d410d387a57
welmoT <36370026+welmoT@users.noreply.github.com>
2020-03-24 21:30:04 +0300

    24.03

--- map/Test_map.java
+++ map/Test_map.java
@@ -2,14 +2,19 @@ class Test_map
 {
 	public static void main(String[] args)
 	{
+		int key,value=0;
 		map M = new map();
-		field F= new field();
-		for(int i = 0; i<12; i++)
+		for(int i = 12; i>0; i--)
 		{
-			F.key=i;
-			F.value=i+3;
-			M.Add(F);
+		
+			key=i;
+			value=3;
+			M.Add(key, value);
 		}
+		//M.print();
+		M.little_right_rotate(M.root.left);
+		//M.little_left_rotate(M.root.right.right);
+		//System.out.println(M.Acess_value(M.root, M.root.key).toString());
 		M.print();
 	}
 }
--- map/map.java
+++ map/map.java
@@ -1,22 +1,34 @@
 
-class field <T extends Comparable<T>>
-{
-	T key;
-	T value;
-
-}
 class node <T extends Comparable<T>>
 { 
-	field field;
+	T key;
+	T value;
 	node left;
 	node right;
 	node parent;
-	node(field field)
+	int height;
+	node()
 	{
-		this.field=field;
 		this.left = null;
 		this.right = null;
 		this.parent = null;
+	}
+	node(T key, T value)
+	{
+		this.key = key;
+		this.value = value;
+		this.left = null;
+		this.right = null;
+		this.parent = null;
+		height=1;
+	}
+	void copy( node <T> N1)
+	{
+		this.key = N1.key;
+		this.value = N1.value;
+		this.left = N1.left;
+		this.right = N1.right;
+		this.parent = N1.parent;
 	}
 }
 
@@ -24,45 +36,71 @@ class map <T extends Comparable<T>>
 {
 	node root;
 	int count;
-	void Add(field field)//T key,T value
+	int height (node <T> Node)//function return heigh
 	{
-		if(this.root==null)
+		if(Node != null) 
+		{
+			return Node.height;
+		}
+		else
+			return 0;
+	}
+	int BF(node <T> Node)
+	{
+		return Node.right.height-Node.left.height;
+	}
+	void Overheigh (node <T> Node)
+	{
+		if(Node.left.height > Node.right.height)  
+			Node.height = Node.left.height + 1;
+		else
+			Node.height = Node.right.height + 1;
+	}
+	void Add(T key, T value)
+	{
+		//System.out.println( key.toString() );
+		if (this.root==null)
         {
-            this.root=new node(field);
+            this.root=new node(key,value);
+          //  System.out.println( key.toString() );
             count++;
         }
         else
         {
-            Addto(this.root, field);
+            Addto(this.root, key,value);
         }
 	}
-	void Addto(node <T> Node, field field)
+	void Addto(node <T> Node, T key, T value)
     {
-        if(field.key.compareTo(Node.field.key) <0)
+        if ( key.compareTo(Node.key) < 0)
         {
            	
-            if(Node.left==null)
+            if(Node.left == null)
             {
-                Node.left=new node(field);
+                Node.left = new node(key, value);
+         		Node.left.parent= Node;
+              // System.out.println(Node.key.toString());
                 count++;
             }
             else
             {
-                Addto(Node.left,field);
+                Addto(Node.left, key, value);
             }
         }
-        else if (field.key.compareTo(Node.field.key)>=0)
+        else if (key.compareTo(Node.key) >= 0)
         {
      
             if(Node.right==null)
             {
-                Node.right=new node(field);
+                Node.right=new node(key, value);
+                Node.right.parent=Node;
+               //System.out.println(Node.key.toString());
                 count++;
             }
             else
             {
 
-                Addto(Node.right,field);
+                Addto(Node.right,key, value);
             }
         }
     }
@@ -74,17 +112,48 @@ class map <T extends Comparable<T>>
         }
         else
         {
-            System.out.println("Tree does not exist");
+            System.out.println( "Tree does not exist" );
         }
     }
-    private void print_tree(node <T> node)
+    private void print_tree( node <T> Node )
     {
-        if(node!=null)
+        if( Node!=null)
         {
-            print_tree(node.left);
-            System.out.println(node.field.key.toString());
-            print_tree(node.right);
+            print_tree( Node.left );
+            System.out.println( Node.key.toString() );
+            print_tree( Node.right );
         }
-        //System.out.println("count "+count);
     }
+    void rotate(node Node)
+    {
+    	//Node.parentlittle_right_rotate(Node);
+    }
+   node <T> little_right_rotate(node <T> Rgh)
+    {
+    	node Lf = new node ();
+    	Lf= Rgh.left;
+    	//Lf.parent=Rgh;
+    	Rgh.left = null;
+    	Rgh.left= Lf.right;
+    	Lf.right=Rgh;
+    	Overheigh(Lf);
+    	Overheigh(Rgh);
+    	return Lf;
+    }
+    node <T> little_left_rotate(node <T> Lf)
+    {
+    	node Rgh = new node ();
+    	Rgh = Lf.right;
+    	Lf.right=Rgh.left;
+    	Rgh.left=Lf;
+    	Overheigh(Lf);
+    	Overheigh(Rgh);
+    	return Rgh;
+    }
+
+    node <T> Balance (node <T> Node)
+    {
+    	return Node;
+    }
+   
 }
