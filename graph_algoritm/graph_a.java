3a4b0ee9bb016558d8b48a0efec46fad5f5eda69
welmoT <36370026+welmoT@users.noreply.github.com>
2020-05-14 17:52:51 +0300

    14.05 edition

--- graph_algoritm/graph_a.java
+++ graph_algoritm/graph_a.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.util.Scanner;
 
 class top 
 {
@@ -16,56 +15,56 @@ class top
 public class graph_a 
 {
 	public static int INF = Integer.MAX_VALUE;
-	int count_ver, count_ver1 = 4; //count_edge - количество ребер count_ver - количество вершин
+	int count_ver, count_ver1 = 4,count_edge; //count_edge - количество ребер count_ver - количество вершин
 	LinkedList <top> [] list_adj; // список смежных вершин
 	int[][] graph;// матрица смежностей
-	boolean directed; // для того чтобы определить направленность. в зависимости от того направленный или нет список смежности будет разный
+	boolean directed; // для того чтобы определить направленность.
+	//boolean[] temp = new boolean[4]; 
+	boolean[] temp;
 	graph_a (int [][] graph)
 	{
 		this.graph = graph;
 	}
-	graph_a (int count/*, int count_edge*/, String s_dir)
+	graph_a (int count, int count_edge, String s_dir)
     {
 		this.count_ver = count;
-		//this.count_ver1 = count;
-		 //System.out.println("h " + count_ver);
-       // this.count_edge = count_edge ; 
-		//this.current_ce = 0; //Это нужно для тогочтобы чмотреть каково текущее число ребер в графе. в тесте ты указываешь какое 
-							//число вершин и ребер у тебя будет в графе, если после create_graph current_ce получилось больше чем заказывали, вызвать эксепшн(я не делала)
-		//el_array = new top[count_el]; //для отслеживания списка ребер (я использовала для алгоритма крускала)
+		this.count_edge = count_edge;
+		//count_ver1 = count;
         list_adj = new LinkedList[count_ver];
-
-		for(int i=0; i < count_ver; i++) list_adj[i] = new LinkedList();
-			
-		if(s_dir=="directed") directed=true;  else directed=false; //weighted в принципе не обязателен, алгоритмы то у нас все равно требуют взвешенный граф, но для полноты можно использовать
+		for(int i=0; i < count_ver; i++) 
+			list_adj[i] = new LinkedList();
+		if(s_dir=="directed") 
+			directed=true;  
+		else directed=false; 
+		temp = new boolean[count_ver];
 		
     }
-	boolean[] temp = new boolean[4]; 
 	void create_graph(int top1, int top2, int weight)
 	{
-			top new_ft = new top(top1,top2,weight);
-
-			if (directed==false)
-			{
-				list_adj[top1].add(new_ft);
-				top new_tf = new top(top2,top1,weight);//Здесь была ошибка. строчки 49-50 нужны для того чтобы ... если граф направленный то запись добавить(0, 1, 5) - говорит о том, что
-														//Нужно сделать ссылку с 0 до 1. Если граф ненаправленный - слыка будет не только с 0 до 1, но и с 1 до 0, причем такого же веса
- 				list_adj[top2].add(new_tf);
-			}
+		top new_ft = new top(top1,top2,weight);
+		if (directed==false)
+		{
+			list_adj[top1].add(new_ft);
+			top new_tf = new top(top2,top1,weight);										
+ 			list_adj[top2].add(new_tf);
+		}
 			
-			else {System.out.println(new_ft.top1+" - "+new_ft.top2);list_adj[top1].add(new_ft);}
-			//el_array [current_ce] = new_ft;
-		//	current_ce++;
+		else 
+		{
+			//System.out.println(new_ft.top1+" - "+new_ft.top2);
+			list_adj[top1].add(new_ft);
+		}
 	}
 
 	public void Print()
     { 
+		System.out.println("adjacent vertex graph");
 		for(int i=0; i < count_ver; i++)
         {
             System.out.print("["+i+"]:");
             for(top C: list_adj[i])
             {
-               System.out.print("  "+C.top2 +" "+"("+C.top1+","+C.top2+","+C.weight+")"); //в скобках записывается полное имя ребра с весом. необязательно. классически только вершины указывать
+               System.out.print("    "+C.top2 +" "+"("+C.top1+","+C.top2+","+C.weight+")"); 
             }
             System.out.println();
         }
@@ -76,8 +75,7 @@ public class graph_a
 
 	public void DFS (int index)// поиск в глубину
 	{
-		
-		System.out.println(temp[0]);
+		//System.out.println("Depth-first search");
 		temp[index] = true;
 		System.out.println(index);
 		 ListIterator<top> i = list_adj[index].listIterator();
@@ -90,6 +88,7 @@ public class graph_a
 	}
 	public void BFS (int index )// поиск в ширину
 	{
+		System.out.println("breadth-first search");
 		boolean [] tmp = new boolean[count_ver];
 		LinkedList<Integer> queue = new LinkedList();
 		queue.add(index);
@@ -111,117 +110,137 @@ public class graph_a
             }
          }
 	}
-/*	boolean check (boolean [] array)
+	public void dijkstra(int start) 
 	{
-		int k = 0;
-		for(int i = 0; i< array.length;i++)
-         {
-         	if(array[i] == false) k++;
-         }
-         if(k = 0) return false;
-         return true;
-	}
-	*/
-	/*public void dijkstra(int start)
-	{
-        boolean [] U = new boolean[count_ver];
-         int[] D = new int[count_ver];
-         for(int i = 0; i< D.length;i++)
-         {
-         	D[i] = INF;
-         	U[i] = false;
-         }
-         D[start] = 0;
-         while(U.check != false)
-        {
-         	for(int i=0; i < count_ver; i++)
-        	{
-            	for(top C: list_adj[i])
-            	{
-
-            	}
-            }
-        }
-
-		
-	}
-   */
-	void dijkstra(int start) {
-		System.out.println("h " + count_ver1);
-         boolean[] used = new boolean [count_ver1]; // массив пометок
-        int[] dist = new int [count_ver1]; // массив расстояния. dist[v] = минимальное_расстояние(start, v)
-         for(int i = 0; i< count_ver1;i++)
-         {
-         	dist[i] = INF;
-         	//U[i] = false;
-         }
-        // fill(dist, INF); // устанаавливаем расстояние до всех вершин INF
-         //System.out.println('h');
-        dist[start] = 0; // для начальной вершины положим 0
-         
-         for (;;) {
-             int v = -1;
-             for (int nv = 0; nv < count_ver1; nv++) // перебираем вершины
-                 if (!used[nv] && dist[nv] < INF && (v == -1 || dist[v] > dist[nv])) // выбираем самую близкую непомеченную вершину
-                     v = nv;
-             if (v == -1) break; // ближайшая вершина не найдена
-             used[v] = true; // помечаем ее
-             for (int nv = 0; nv < count_ver1; nv++)
-                 if (!used[nv] && graph[v][nv] < INF) // для всех непомеченных смежных
-                     dist[nv] = Math.min(dist[nv], dist[v] + graph[v][nv]); // улучшаем оценку расстояния (релаксация)
-         }
-         for(int i = 0; i< count_ver1; i++)
-         {
-         	System.out.println("dist"+i+dist[i]);
-         }
+    System.out.println("algoritm Dijkstra");
+    boolean[] used = new boolean [count_ver1]; // массив пометок
+    int[] dist = new int [count_ver1]; // массив расстояния. dist[v] = минимальное_расстояние(start, v)
+    for(int i = 0; i < count_ver1; i++)
+      dist[i] = INF;
+    dist[start] = 0;
+    for (;;) 
+    {
+      int v = -1;
+      for (int i = 0; i < count_ver1; i++) 
+        if (!used[i] && dist[i] < INF && (v == -1 || dist[v] > dist[i])) 
+          v = i;
+        if (v == -1) break; 
+          used[v] = true; 
+      for (int i = 0; i < count_ver1; i++)
+        if (!used[i] && graph[v][i] < INF) 
+          dist[i] = Math.min(dist[i], dist[v] + graph[v][i]); 
     }
+    for(int i = 0; i< count_ver1; i++)
+      System.out.println("for "+start+" "+"to " +i+": "+" weight "+dist[i]);
+  }
+  public void Prim() 
+  {
+    System.out.println("algoritm Prim");
+    int  temp1, temp2;
+    boolean[] used = new boolean[count_ver1];
+    int[] parent = new int[count_ver1];
+    used[0] = true;
+    for ( int k = 1; k < count_ver1; k++ )
+    {
+      used[k] = false;
+    }
+    parent[0] = 0;
+    for (int k = 1; k < count_ver1; k++)
+    {
+      temp1 = temp2 = 0;
+      for ( int i = 0; i < count_ver1; i++ )
+        for ( int j = 0; j < count_ver1; j++ )
+        {
+          if ( used[i] && !used[j] &&graph[i][j] < graph[temp1][temp2])
+          {
+            temp1 = i;
+            temp2 = j;
+          }
+        }
+        //System.out.println("Min cost edge: (" + + temp1 + "," + + temp2+ ")" +"cost = " + graph[temp1][temp2]);
+      parent[temp2] = temp1;
+      used[temp2] = true;
+    }
+    int[] a = parent;
+   	for ( int i = 0; i < count_ver1; i++ )
+      System.out.println( "for: "+a[i] + " - "+"to "+ i );
+  }
 
-    public void Prim( )
-   {
-      int i, j, k, x, y;
-      boolean[] Reached = new boolean[count_ver1];
-      int[] predNode = new int[count_ver1];
-      Reached[0] = true;
-      for ( k = 1; k < count_ver1; k++ )
+  public void kruskal()//https://algorithms.tutorialhorizon.com/kruskals-algorithm-minimum-spanning-tree-mst-complete-java-implementation/
+  {
+    System.out.println("algoritm Kruskal");
+    PriorityQueue<top> queue = new PriorityQueue<>(count_edge, Comparator.comparingInt(c -> c.weight));
+    for(int i=0; i < count_ver1; i++)
+    {
+      for(top C: list_adj[i])
       {
-         Reached[k] = false;
+        queue.add(C);
       }
-      predNode[0] = 0;
-     // printReachSet( Reached );
-      for (k = 1; k < count_ver1; k++)
+    }
+    int [] parent = new int[count_ver1];
+    parent(parent);
+		ArrayList<top> mst = new ArrayList<>();
+    int index = 0;
+    while(index < count_ver1 - 1)
+    {
+      top edge = queue.remove();
+      int x_set = find(parent, edge.top1);
+      int y_set = find(parent, edge.top2);
+      if(x_set != y_set)
       {
-         x = y = 0;
-         for ( i = 0; i < count_ver1; i++ )
-            for ( j = 0; j < count_ver1; j++ )
-            {
-                if ( Reached[i] && !Reached[j] &&
-                     graph[i][j] < graph[x][y] )
-                {
-                   x = i;
-                   y = j;
-                }
-            }
-         System.out.println("Min cost edge: (" +
-                                + x + "," +
-                                + y + ")" +
-                                "cost = " + graph[x][y]);
-         predNode[y] = x;
-         Reached[y] = true;
-         //printReachSet( Reached );
-         System.out.println();
+        mst.add(edge);
+        index++;
+        union(parent,x_set,y_set);
       }
-      int[] a= predNode;
-   for ( i = 0; i < count_ver1; i++ )
-          System.out.println( a[i] + " --> " + i );
-   }
-  /* void printReachSet(boolean[] Reached )
-   {
-      System.out.print("ReachSet = ");
-      for (int i = 0; i < Reached.length; i++ )
-         if ( Reached[i] )
-           System.out.print( i + " ");
-      //System.out.println();
-   }
-   */
+    }
+      printGraph(mst);
+  }
+
+  public void parent(int [] parent)
+  {
+    for (int i = 0; i < count_ver1 ; i++) 
+    {
+      parent[i] = i;
+    }
+  }
+  public int find(int [] parent, int top)
+  {
+    if(parent[top] != top)
+      return find(parent, parent[top]);;
+    return top;
+  }
+  public void union(int [] parent, int x, int y)
+  {
+    int x_set_parent = find(parent, x);
+    int y_set_parent = find(parent, y);
+    parent[y_set_parent] = x_set_parent;
+  }
+  public void printGraph(ArrayList<top> edgeList)
+  {
+    for (int i = 0; i <edgeList.size() ; i++) 
+    {
+      top edge = edgeList.get(i);
+      System.out.println("Edge-" + i + " for: " + edge.top1 +" to: " + edge.top2 +" weight: " + edge.weight);
+    }
+  }
+ 		public void Floid ()
+ 		{
+      System.out.println("Floid algoritm");
+ 			for(int k = 0; k < count_ver1; k++)
+ 			{
+ 				for (int i = 0;i< count_ver1; i++) 
+ 				{
+ 					for(int j = 0; j<count_ver1;j++)
+ 					{
+ 						graph[i][j] = Math.min(graph[i][j],graph[i][k]+graph[k][j]);
+ 					}
+ 				}
+ 			}
+      for (int i = 0;i < graph.length;i++ ) 
+      {
+        graph[i][i] = 0;
+      }
+      System.out.println(Arrays.deepToString(graph));
+ 		}
 }
  
--- graph_algoritm/test_graph_a.java
+++ graph_algoritm/test_graph_a.java
@@ -3,35 +3,25 @@ public class test_graph_a < T extends Comparable <T> >
 {
 	public static void main(String[] args) 
 	{
-		graph_a G = new graph_a(4/*,5*/,"directed");
-		int [][] array = {{100,100,1,5},{4,100,3,100},{100,100,100,1},{2,2,100,100}}; 
+	    int INF = 10000;
+		graph_a G = new graph_a(4,8,"directed");
+		int [][] array = {{INF,INF,1,5},{4,INF,3,INF},{INF,INF,INF,1},{2,2,INF,INF}}; 
 		graph_a G1 = new graph_a (array);
-		//ArrayList Q1 = new ArrayList();
-		//Q1.add(2);
-		//int [] a0 = {1,2,0,2,1};
-		//int [] a1 = {2,2,1};
-		//Q1.add(a0);
-		//Q1.add(a1);
-		//Q1.add(a2);
-		//G.create_graph(0,2,10);
-		G.create_graph(1,2,4);
+		G.create_graph(0,3,7);
+		G.create_graph(0,2,10);
+		G.create_graph(1,2,1);
+		G.create_graph(1,3,4);
+		G.create_graph(1,0,15);
 		G.create_graph(2,3,8);
 		G.create_graph(3,0,23);
-		G.create_graph(3,1,0);
-		G.create_graph(1,0,0);
-		G.create_graph(0,3,0);
+		G.create_graph(3,1,9);
 		G.Print();
-		int i = 0;
-		G.BFS(i);
-		G.DFS(i);
+		G.BFS(0);
+		System.out.println("\nDepth-first search");
+		G.DFS(0);
+		G.kruskal();
 		G1.dijkstra(0);
 		G1.Prim();
-		//G.print_listedges();
-		//G.create_graph(3,1,0);
-
-
-		//G.DFC(0);
-
-
+		G1.Floid();
 	}
 }
